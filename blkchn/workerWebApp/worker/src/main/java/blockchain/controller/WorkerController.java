cda1d85bab4352a8f16e9d9e179f91d474df59a8
david <davidndy@gmail.com>
2019-10-02 10:37:49 -0500

    add webclient to make Http requests

--- blkchn/workerWebApp/worker/src/main/java/blockchain/controller/WorkerController.java
+++ blkchn/workerWebApp/worker/src/main/java/blockchain/controller/WorkerController.java
@@ -15,7 +15,8 @@ public class WorkerController {
   
   @GetMapping("/mine")
   public String Mine(String[] args) {
-    //background job? If for any reason the services needs to be available for another queries maybe not.
+    // background job? If for any reason the services needs to be available for another queries such as balance?
+	// maybe not.
     worker.mine();
     
     return "Finish mining";
