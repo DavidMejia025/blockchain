c32ce4424284dbc79764be6ea40ee8fa3bcd263d
david <davidndy@gmail.com>
2019-10-03 20:43:33 -0500

    complete the first version of the client CLApp

--- blkchn/CLA/src/main/java/client/CLA/Client.java
+++ blkchn/CLA/src/main/java/client/CLA/Client.java
@@ -9,69 +9,84 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import client.CLA.utils.Logs;
 import client.CLA.utils.ParameterStringBuilder;
 
 public class Client {
     public static void main( String[] args ) throws IOException {
-    	Logs logs            = new Logs();
+    	Logs   logs = new Logs();
+    	String msj;
+    	String inputText;
     	String senderAddress = "AS23245#%%3vFG43HD234@#$%$%234";
     	
-    	logs.addLog("Hi #name would you like to create a new transaction?");
+    	logs.addMessage("Welcome to Wallet.io.");
+    	logs.addMessage("Please Log in:");
+
+        String userName = askUserInformation(logs, "Type your user name");
+
+    	String password = askUserInformation(logs, "Type your password");
     	
-    	logs.addLog("Welcome to Wallet.io.");
-    	logs.addLog("Please Log in:");
+    	//validation To be implemented
     	
-    	//To be implemented
+    	msj = "Hi "+ userName + " would you like to create a new transaction? (yes/no)";
+    	inputText = askUserInformation(logs, msj);
     	
-    	logs.addLog("Hi #name would you like to create a new transaction?");
-    	
-    	if (true) {
-    		createNewTransaction(senderAddress);
+    	if (inputText.equals("yes")) {
+    		createNewTransaction(logs, senderAddress);
     	}else {
-    		logs.addLog("Do you want to see your balance?");
-    		if (true) {
-    			//show balance
-    			logs.addLog("Hi #name would you like to create a new transaction?");
-    			if (true) {
-    				createNewTransaction(senderAddress);	
+    		inputText = askUserInformation(logs, "Do you want to see your balance?");
+    		if (inputText.equals("yes")) {
+    			logs.addMessage("0 pending transactions; total X Coins = 0");
+    			logs.addBlankLine();
+    			
+    			msj = userName + " may I helpo you creating a new transaction? (yes/no)";
+    	    	inputText = askUserInformation(logs, msj);
+    	    	
+    			if (inputText.equals("yes")) {
+    				createNewTransaction(logs, senderAddress);	
     			}else {
-    				//log out and end process
+    				logs.addMessage("Thank you for using Wallet.io we are happy helping you out, see you next time :)!.");
     			}    				
     		}else {
-    			//log out and end process
+    			logs.addMessage("Thank you for using Wallet.io we are happy helping you out, see you next time :)!.");
     		}
     		// 
     	}    	
     }
     
-    public static void createNewTransaction(String address) throws IOException {
+    public static void createNewTransaction(Logs logs, String address) throws IOException {
     	String senderAddress = address;
-    	Logs logs = new Logs();
+    	String inputText;
     	
-    	logs.addLog("Please type the recipent address below:");
-    	String recipentAddress = "324FDFVDRG%%$f2345FVFF";
+    	logs.addBlankLine();
+    	String recipentAddress = askUserInformation(logs, "Please type the recipent address below:");
     	
-    	logs.addLog("Add the desired amount of X Coins that you want to transfer:");
-    	int value = 100;
+    	String msj = "Add the desired amount of X Coins that you want to transfer:";
+    	String value = askUserInformation(logs, msj);
     	
-    	logs.addLog("Transaction information");
-    	logs.addLog("Sending " + value + " X Coins to " + recipentAddress + "Confirm transaction?");
+    	logs.addBlankLine();
+    	logs.addMessage("Transaction information");
+    	logs.addBlankLine();
+    	logs.addMessage("Sending " + value + " X Coins to " + recipentAddress);
+    	inputText = askUserInformation(logs, " Confirm transaction? (yes/no) ");
     	
-    	if (true) {
-    		sendTransaction(senderAddress, recipentAddress, value);
+    	if (inputText.equals("yes")) {
+    		sendTransaction(logs, senderAddress, recipentAddress, value);
     	}else {
-    		logs.addLog("Create new transaction");
-    		if (true) {
-    			createNewTransaction(senderAddress);	
+    		logs.addMessage("Do you want to create a new transaction? (y/n)");
+    		inputText = askUserInformation(logs, "Do you want to create a new transaction? (y/n)");
+    		
+    		if (inputText.equals("yes")) {
+    			createNewTransaction(logs, senderAddress);	
     		}    		
     	}
     	
-    	logs.addLog("Thank you for using Wallet.io we are happy helping you out, see you next time.");
+    	logs.addMessage("Thank you for using Wallet.io we are happy helping you out, see you next time :)!.");
     }
     
-    public static void sendTransaction(String sender, String recipent, Integer value) throws IOException {
+    public static void sendTransaction(Logs logs, String sender, String recipent, String value) throws IOException {
     	URL url;
     	
 		try {
@@ -84,7 +99,7 @@ public class Client {
 			
 			parameters.put("sender", sender);
 			parameters.put("recipent", recipent);
-			parameters.put("value", "10");
+			parameters.put("value", value);
 			
 			con.setDoOutput(true);
 			DataOutputStream out = new DataOutputStream(con.getOutputStream());
@@ -102,8 +117,7 @@ public class Client {
 			    content.append(inputLine);
 			}
 			
-			System.out.println(".............................................");
-			System.out.println(content);
+			logs.addLog("node status: " + status);
 			
 			in.close();
 			con.disconnect();
@@ -111,6 +125,15 @@ public class Client {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    }
+    
+    public static String askUserInformation(Logs logs, String msj) {
+    	Scanner sc  = new Scanner(System.in);
+    	
+    	logs.addMessage(msj);
+    	String inputText = sc.next();
+        
+    	return inputText;
     }
 }
 
--- blkchn/CLA/src/main/java/client/CLA/utils/Logs.java
+++ blkchn/CLA/src/main/java/client/CLA/utils/Logs.java
@@ -13,6 +13,14 @@ public class Logs {
     System.out.println(preMessage() + msj + postMessage());
   }
   
+  public void addMessage(String msj) {
+    System.out.println(">>>>  " + msj + "  <<<<");
+  }
+  
+  public void addBlankLine() {
+	  System.out.println(" ");
+  }
+  
   private String preMessage() {
     Date dateobj = new Date();  
     
